9f33f404a9735c841c113ec446f9d49dc97ee582
Kazpenn <kazpenn2020@gmail.com>
2021-03-18 10:02:57 -0600

    Completed parser with ddl output.

--- src/main/java/edu/cnm/deepdive/parser/model/ForeignKey.java
+++ src/main/java/edu/cnm/deepdive/parser/model/ForeignKey.java
@@ -1,7 +1,63 @@
 package edu.cnm.deepdive.parser.model;
 
+import com.google.gson.annotations.Expose;
+import java.util.List;
+
 public class ForeignKey {
 
-  // TODO Add fields, accessors, mutators, and @Expose annotations
+  @Expose
+  private String table;
+
+  @Expose
+  private String onDelete;
+
+  @Expose
+  private String onUpdate;
+
+  @Expose
+  private List<String> columns;
+
+  @Expose
+  private List<String> referenceColumn;
+
+  public String getTable() {
+    return table;
+  }
+
+  public void setTable(String table) {
+    this.table = table;
+  }
+
+  public String getOnDelete() {
+    return onDelete;
+  }
+
+  public void setOnDelete(String onDelete) {
+    this.onDelete = onDelete;
+  }
+
+  public String getOnUpdate() {
+    return onUpdate;
+  }
+
+  public void setOnUpdate(String onUpdate) {
+    this.onUpdate = onUpdate;
+  }
+
+  public List<String> getColumns() {
+    return columns;
+  }
+
+  public void setColumns(List<String> columns) {
+    this.columns = columns;
+  }
+
+  public List<String> getReferenceColumn() {
+    return referenceColumn;
+  }
+
+  public void setReferenceColumn(List<String> referenceColumn) {
+    this.referenceColumn = referenceColumn;
+  }
 
 }
